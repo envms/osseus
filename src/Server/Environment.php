2ec97fc99e420045e72791ac20cb2c9cc34a76da
cbornhoft <chris@env.ms>
2018-10-18 09:12:44 -0400

    Add Environment class

---
+++ src/Server/Environment.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Envms\Osseus\Server;
+
+use Envms\Osseus\Architecture\Singleton;
+
+/**
+ * Class Environment
+ *
+ * The purpose of this class is to set a current runtime environment that can be used across the application
+ */
+class Environment extends Singleton
+{
+
+    public const DEVELOPMENT = 1;
+    public const NIGHTLY = 2;
+    public const STAGING = 3;
+    public const PRODUCTION = 4;
+
+    /** @var int */
+    protected $previous = null;
+    /** @var int */
+    protected $current = null;
+
+    /**
+     * @return Environment|mixed
+     */
+    public static function instance()
+    {
+        return parent::instance();
+    }
+
+    /**
+     * @param int $environment
+     */
+    public function init(int $environment): void
+    {
+        $this->current = $environment;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPrevious(): int
+    {
+        return $this->previous;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCurrent(): int
+    {
+        return $this->current;
+    }
+
+    /**
+     * @param int $environment
+     */
+    public function setCurrent(int $environment): void
+    {
+        $this->previous = $this->current;
+        $this->current = $environment;
+    }
+
+}
