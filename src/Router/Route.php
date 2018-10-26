f25523cd162749560c09b4960d2859c8c3bf398f
cbornhoft <chris@env.ms>
2018-10-26 09:59:17 -0400

    Update Route to use DoesNotExist exception

--- src/Router/Route.php
+++ src/Router/Route.php
@@ -3,7 +3,7 @@
 namespace Envms\Osseus\Router;
 
 use Envms\Osseus\Interfaces\Router\Route as RouteInterface;
-use Envms\Osseus\Exception\NotFound;
+use Envms\Osseus\Exception\DoesNotExist;
 use Envms\Osseus\Parse\Uri;
 use Envms\Osseus\Security\Validate;
 
@@ -32,13 +32,13 @@ class Route implements RouteInterface
      * @param  Uri    $uri
      * @param  string $applicationName - The base applicationName's namespace
      *
-     * @throws NotFound
+     * @throws DoesNotExist
      *
      * @return mixed
      */
     public function go(Uri $uri, string $applicationName)
     {
-        $controller = "{$applicationName}\\{$uri->getModule()}\Controller";
+        $controller = "{$applicationName}\\{$uri->getModule()}\\Controller";
         $action = $uri->getAction();
 
         $validate = new Validate($action);
@@ -60,7 +60,7 @@ class Route implements RouteInterface
      * @param  array        $params
      * @param  array|string $options
      *
-     * @throws NotFound
+     * @throws DoesNotExist
      *
      * @return mixed
      */
@@ -71,7 +71,7 @@ class Route implements RouteInterface
         if (!isset($this->instance[$controller])) {
             // if not, try to instantiate the controller
             if (!class_exists($controller)) {
-                throw new NotFound('Controller class not found');
+                throw new DoesNotExist('Controller class not found');
             }
 
             $this->instance[$controller] = new $controller($params, $options);
@@ -79,7 +79,7 @@ class Route implements RouteInterface
 
         // check if controller method can be called
         if (!is_callable([$this->instance[$controller], $action])) {
-            throw new NotFound('Controller method not found');
+            throw new DoesNotExist('Controller method not found');
         }
 
         // call method in requested controller and return output when applicable
