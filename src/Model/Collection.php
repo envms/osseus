fcc1cb312015e6bd6472c562497147f2a3b595ee
cbornhoft <chris@env.ms>
2018-11-01 08:57:08 -0400

    Add methods count(), getModelName(), add() and addMany()

--- src/Model/Collection.php
+++ src/Model/Collection.php
@@ -101,4 +101,55 @@ abstract class Collection implements CollectionInterface
     {
         return isset($this->models[$this->position]);
     }
+
+    /**
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->models);
+    }
+
+    /**
+     * @throws \ReflectionException
+     *
+     * @return string
+     */
+    public function getModelName()
+    {
+        $reflection = new \ReflectionClass($this);
+
+        return $reflection->getNamespaceName() . '\Model';
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return int
+     */
+    public function add(array $data): int
+    {
+        $position = $this->count();
+        $this->models[$position] = new $this->modelName($data);
+
+        return $position;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return int
+     */
+    public function addMany(array $data): int
+    {
+        $position = $this->count();
+
+        foreach ($data as $modelData) {
+            $this->models[$position++] = new $this->modelName($modelData);
+        }
+
+        $modelsAdded = $position - $this->count();
+
+        return $modelsAdded;
+    }
 }
